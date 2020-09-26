b97aaba59c7c27d330534030c41735087d0fb79e
julasweta <julasweta@ukr.net>
2020-09-26 23:33:50 +0300

    header

--- deli.js
+++ deli.js
@@ -1,32 +1,136 @@
-var gulp = require('gulp');
+// подключение файлов стилей и шрифтов
+@import '../css/reset';
+@import '../../node_modules/normalize.css/normalize';
+@import url('https://fonts.googleapis.com/css2?family=Oregano&family=PT+Serif&family=Roboto&display=swap');
 
-var sass = require('gulp-sass');
-var browserSync = require('browser-sync').create();
+//переменные
+$oregano: Oregano,
+    cursive;
+$roboto: Roboto,
+    sans - serif;
+$myriad: myriad - pro,
+    sans - serif;
+
+//убираем нижнюю прокрутку
+html {
+    overflow - x: hidden;
+}
+
+a {
+    text - decoration: none;
+}
+
+body {
+    background - color: #ffffff;
+    font - family: $oregano;
+    font - size: 87.5 % ;
+
+}
+
+
+.header {
+    background - image: url('../images/src/header.jpg');
+    width: 100 % ;
+    height: 100 vh;
+
+    background - repeat: no - repeat;
+    background - size: cover;
+
+}
+
+.header_top {
+    background - color: transparentize(#252a35, .3);
+
+    width: 100%;
+
+    height: 100px;
+
+    position: relative;
+
+    display: inline-block;
+
+}
 
 
 
-gulp.task('sass', function () {
-    return gulp.src("./app/scss/*.scss")
-        .pipe(sass({
-            includePaths: require('node-normalize-scss').includePaths
-        }))
-        .pipe(gulp.dest("./app/css"))
-        .pipe(browserSync.stream());
-});
+.logo {
 
 
 
-gulp.task('browser-sync', function () {
-    browserSync.init({
-        server: {
-            baseDir: "./app/"
-        }
-    });
-});
+    position: static;
 
-gulp.task('watch', gulp.series('browser-sync',
-    'sass',
-    function () {
-        gulp.watch('./app/scss/**/*scss', ['sass']);
-        gulp.watch("app/*.html").on("change", reload);
-    }));
+    color: white;
+
+    z-index: -1;
+
+    padding-top: 2%;
+
+    padding-left: 4%;
+
+
+
+}
+
+
+
+.logotyp {
+
+    background-image: url('../images/src/logo.png');
+
+    height: 100px;
+
+    width: auto;
+
+    background-repeat: no-repeat;
+
+    z-index: -1;
+
+}
+
+
+
+.menu {
+
+    font-family: $roboto;
+
+
+
+    position: fixed;
+
+    left: 30%;
+
+    margin-top: -7%;
+
+
+
+    ol li {
+
+        display: inline-block;
+
+
+
+        a {
+
+            color: rgb(255, 255, 255);
+
+            margin-right: 3.3em;
+
+
+
+            &:hover {
+
+                border-bottom: 2px solid # 0b e60b;
+    }
+}
+}
+}
+.hotline {
+    width: 50 % ;
+    position: absolute;
+    top: 5 % ;
+    left: 70 % ;
+    display: inline - block;
+
+}
+
+
--- gulpfile.js
+++ gulpfile.js
@@ -13,31 +13,17 @@ const {
 
 // Подключаем Browsersync
 const browserSync = require('browser-sync').create();
-
-// Подключаем gulp-concat
 const concat = require('gulp-concat');
-
-// Подключаем gulp-uglify-es
 const uglify = require('gulp-uglify-es').default;
-
-// Подключаем модули gulp-sass и gulp-less
 const sass = require('gulp-sass');
 const less = require('gulp-less');
-
-// Подключаем Autoprefixer
 const autoprefixer = require('gulp-autoprefixer');
-
-// Подключаем модуль gulp-clean-css
 const cleancss = require('gulp-clean-css');
+const imagemin = require('gulp-imagemin');
+const newer = require('gulp-newer');
+const del = require('del');
 
-	// Подключаем gulp-imagemin для работы с изображениями
-	const imagemin = require('gulp-imagemin');
 
-	// Подключаем модуль gulp-newer
-	const newer = require('gulp-newer');
-
-	// Подключаем модуль del
-	const del = require('del');
 
 
 // Определяем логику работы Browsersync
@@ -52,10 +38,11 @@ function browsersync() {
 }
 
 
-//Работа со скриптами
+// JS Работа со скриптами
 function scripts() {
   return src([ // Берём файлы из источников
-      'node_modules/jquery/dist/jquery.min.js', // Пример подключения библиотеки
+      'node_modules/@fortawesome/fontawesome-free/js/fontawesome.js',
+      //'node_modules/jquery/dist/jquery.min.js', // Пример подключения библиотеки
       'app/js/app.js', // Пользовательские скрипты, использующие библиотеку, должны быть подключены в конце
     ])
     .pipe(concat('app.min.js')) // Конкатенируем в один файл
@@ -64,28 +51,23 @@ function scripts() {
     .pipe(browserSync.stream()) // Триггерим Browsersync для обновления страницы
 }
 
-//создадим новую функцию startwatch(), которая запустит наблюдение за изменениями файлов.
-function startwatch() {
 
-  // Выбираем все файлы JS в проекте, а затем исключим с суффиксом .min.js
-  watch(['app/**/*.js', '!app/**/*.min.js'], scripts);
 
-}
-
+//STYLES
 function styles() {
   return src('app/' + preprocessor + '/styles.scss' + '') // Выбираем источник: "app/sass/styles.scss"
     .pipe(eval(preprocessor)()) // Преобразуем значение переменной "preprocessor" в функцию
     .pipe(concat('app.min.css')) // Конкатенируем в файл app.min.js
-    .pipe(autoprefixer({
+    .pipe(autoprefixer({ // Создадим префиксы с помощью Autoprefixer
       overrideBrowserslist: ['last 10 versions'],
       grid: true
-    })) // Создадим префиксы с помощью Autoprefixer
+    }))
     .pipe(cleancss({
       level: {
         1: {
           specialComments: 0
         }
-      } /* , format: 'beautify' */
+      }
     })) // Минифицируем стили
     .pipe(dest('app/css/')) // Выгрузим результат в папку "app/css/"
     .pipe(browserSync.stream()) // Сделаем инъекцию в браузер
@@ -93,7 +75,7 @@ function styles() {
 
 
 
-//
+// IMG
 function images() {
   return src('app/images/src/**/*') // Берём все изображения из папки источника
     .pipe(newer('app/images/dest/')) // Проверяем, было ли изменено (сжато) изображение ранее
@@ -102,37 +84,30 @@ function images() {
 }
 
 
-//
+// CLEAN
 function cleanimg() {
   return del('app/images/dest/**/*', {
     force: true
   }) // Удаляем всё содержимое папки "app/images/dest/"
 }
-//
-function startwatch() {
 
-  // Выбираем все файлы JS в проекте, а затем исключим с суффиксом .min.js
-  watch(['app/**/*.js', '!app/**/*.min.js'], scripts);
-  // Мониторим файлы препроцессора на изменения
-  watch('app/**/' + preprocessor + '/**/*', styles);
-  // Мониторим файлы HTML на изменения
-  watch('app/**/*.html').on('change', browserSync.reload);
+
+
+// WATCH создадим новую функцию startWatch(), которая запустит наблюдение за изменениями файлов.
+// Выбираем все файлы JS в проекте, а затем исключим с суффиксом .min.js
+// Мониторим файлы препроцессора на изменения
+// Мониторим файлы HTML на изменения
+function startwatch() {
+ watch(['app/**/*.js', '!app/**/*.min.js'], scripts);
+ watch('app/**/' + preprocessor + '/**/*', styles);
+ watch('app/**/*.html').on('change', browserSync.reload);
 }
 
 // Экспортируем функцию browsersync() как таск browsersync. Значение после знака = это имеющаяся функция.
 exports.browsersync = browsersync;
-
-// Экспортируем функцию scripts() в таск scripts
 exports.scripts = scripts;
-
-// Экспортируем функцию styles() в таск styles
 exports.styles = styles;
+exports.images = images;
+exports.cleanimg = cleanimg;
 
-	// Экспорт функции images() в таск images
-	exports.images = images;
-
-  	// Экспортируем функцию cleanimg() как таск cleanimg
-  	exports.cleanimg = cleanimg;
-
-// Экспортируем дефолтный таск с нужным набором функций
 exports.default = parallel(styles, scripts, browsersync, startwatch);
