46394f24f670d88c5e0b42d7b347aad143dc998d
julasweta <julasweta@ukr.net>
2020-10-04 20:48:23 +0300

    add_stories_blogs_start_courses_blockes

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,4 @@
+// Carousel Swiper
  var swiper = new Swiper('.swiper-container', {
   slidesPerView: 1,
     spaceBetween: 30,
@@ -5,10 +6,28 @@
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
-    }, 
+    },
    navigation: {
      nextEl: '.swiper-button-next',
      prevEl: '.swiper-button-prev',
    },
  });
 console.log('PRIVET');
+
+//Form
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+
+
+// HTML
+var price = document.getElementsByClassName('prise');
+console.log(price[0].textContent);
+for(i=0; i<3;i++)
+if (price[i].textContent == 'free') {
+  price[i].style.color = 'rgb(30, 175, 19)';
+}
